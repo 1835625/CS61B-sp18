a470779c510b2a9b69f34d3e5211e4664dc5ad29
Wei Liu <mavrick1806@gmail.com>
2026-02-05 11:49:01 +0800

    Complete and pass basic methods before section update

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,4 +1,5 @@
 public class Planet {
+    static final double G = 6.67e-11;
     public double xxPos;
     public double yyPos;
     public double xxVel;
@@ -23,5 +24,48 @@ public class Planet {
         yyVel = p.yyVel;
         mass = p.mass;
         imgFileName = p.imgFileName;
+    }
+
+    public double calcDistance(Planet p) {
+        return Math.sqrt(Math.pow((p.xxPos - xxPos), 2) + Math.pow((p.yyPos - yyPos), 2));
+    }
+
+    public double calcForceExertedBy(Planet p) {
+        double Distance = this.calcDistance(p);
+        return G * mass * p.mass / Math.pow(Distance, 2);
+    }
+
+    public double calcForceExertedByX(Planet p) {
+        double dx = p.xxPos - xxPos;
+        double Distance = this.calcDistance(p);
+        double Force = this.calcForceExertedBy(p);
+        return Force * dx / Distance;
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        double dy = p.yyPos - yyPos;
+        double Distance = this.calcDistance(p);
+        double Force = this.calcForceExertedBy(p);
+        return Force * dy / Distance;
+    }
+
+    public double calcNetForceExertedByX(Planet[] planets) {
+        double NetForceX = 0;
+        for(Planet p : planets) {
+            if(!this.equals(p)) {
+                NetForceX += this.calcForceExertedByX(p);
+            }
+        }
+        return NetForceX;
+    }
+
+    public double calcNetForceExertedByY(Planet[] planets) {
+        double NetForceY = 0;
+        for(Planet p : planets) {
+            if(!this.equals(p)) {
+                NetForceY += this.calcForceExertedByY(p);
+            }
+        }
+        return NetForceY;
     }
 }
